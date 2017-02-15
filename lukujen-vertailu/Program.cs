13615cd650b26efd83c02385ee1a04247d1a5d4e
eaaho <esa.aho@student.saimia.fi>
2017-02-15 09:53:33 +0200

    Lukujen vertailuohjelma

---
+++ lukujen-vertailu/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lukujen_vertailu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Annappa luku: ");
+            int intNro = int.Parse(Console.ReadLine());
+            
+
+            if (intNro > 0)
+            {
+                Console.WriteLine("Luku on " + (intNro.ToString()) + " ja se on positiivinen.");
+            }
+
+            if (intNro < 0)
+            {
+                Console.WriteLine("Luku on " + (intNro.ToString()) + " ja se on negatiivinen.");
+            }
+
+            if (intNro == 0)
+            {
+                Console.WriteLine("Luku on 0.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
